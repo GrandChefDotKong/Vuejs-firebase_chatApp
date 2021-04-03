06769b7a577c8ce897a765c81ba2733c545790ad
GrandChefDotKong <55345801+GrandChefDotKong@users.noreply.github.com>
2021-04-03 14:24:48 +0900

    Update db.js

--- src/data/db.js
+++ src/data/db.js
@@ -2,13 +2,7 @@ import firebase from "firebase";
 
 const config = {
     //API KEYS
-    apiKey: "AIzaSyDSCSm2fJPfEaUBpwBhwnPZXHx1roqYm2U",
-    authDomain: "vue-chat-4ca67.firebaseapp.com",
-    projectId: "vue-chat-4ca67",
-    storageBucket: "vue-chat-4ca67.appspot.com",
-    messagingSenderId: "645706266743",
-    appId: "1:645706266743:web:a35bc658d682d1f03d6e72",
-    measurementId: "G-2SV368K164"
+    // add here your API KEY from firebase
 }
 
 const db = firebase.initializeApp(config);
